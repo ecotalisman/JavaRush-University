168946c1c501e92027240f9cc4fb80a5b602c57e
ecotalisman <ecotalisman@gmail.com>
2023-04-20 18:13:43 +0300

    Add solution for task 701, 702, 703, 704

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,23 @@
+package ua.javarush.task.pro.task07.task0701;
+
+/* 
+Усі можливі типи
+Як тобі вже відомо, загалом у мові Java є 8 примітивних типів.
+Створи по одній змінній кожного примітивного типу в класі Solution.
+Змінні не повинні міститися всередині якихось методів.
+
+Requirements:
+1. У класі Solution мають бути створені змінні всіх примітивних типів.
+*/
+
+public class Solution {
+    //напишіть тут ваш код
+    byte b;
+    short s;
+    int i;
+    long l;
+    double d;
+    float f;
+    char c;
+    boolean bool;
+}
