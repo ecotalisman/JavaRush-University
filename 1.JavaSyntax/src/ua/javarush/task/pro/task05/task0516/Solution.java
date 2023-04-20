32f9cba22c0cccbc50f011ecebbc24b9ca093c1d
ecotalisman <ecotalisman@gmail.com>
2023-04-20 14:40:10 +0300

    Add solution for task 514, 515, 516

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task05/task0516/Solution.java
@@ -0,0 +1,19 @@
+package ua.javarush.task.pro.task05.task0516;
+
+import java.util.Arrays;
+
+/* 
+Заповнюємо масив
+*/
+
+public class Solution {
+
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+        System.out.println(Arrays.toString(array));
+    }
+}
