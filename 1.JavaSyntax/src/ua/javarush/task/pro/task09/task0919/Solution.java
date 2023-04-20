e2aef6ec8144c404279ae2de4390b08b8ca21338
ecotalisman <ecotalisman@gmail.com>
2023-04-20 03:01:18 +0300

    done solution for task 919

--- 1.JavaSyntax/src/ua/javarush/task/pro/task09/task0919/Solution.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task09/task0919/Solution.java
@@ -2,6 +2,12 @@ package ua.javarush.task.pro.task09.task0919;
 
 /* 
 Перевертання рядка
+Використовуючи StringBuilder в методі reverseString(String), переверни рядок, отриманий як параметр.
+
+
+Requirements:
+1. У методі reverseString(String) потрібно використовувати StringBuilder.
+2. Метод reverseString(String) має повертати перевернутий рядок.
 */
 
 public class Solution {
@@ -13,6 +19,10 @@ public class Solution {
 
     public static String reverseString(String string) {
         //напишіть тут ваш код
-        return null;
+        StringBuilder builder = new StringBuilder();
+        builder.append(string);
+        builder.reverse();
+        String str = builder.toString();
+        return str;
     }
 }
