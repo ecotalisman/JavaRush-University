8ef8b0653dd3d0b7d05bc173d02c2632c9cc729d
ecotalisman <ecotalisman@gmail.com>
2023-07-11 16:14:42 +0300

    Add solution for task 1210

---
+++ 2.JavaCore/src/ua/javarush/task/jdk13/task12/task1210/Solution.java
@@ -0,0 +1,29 @@
+package ua.javarush.task.jdk13.task12.task1210;
+
+/* 
+Виконуємо перевантаження!
+Напиши два методи: print(int) та print(String).
+
+Requirements:
+1. Програма не повинна виводити текст на екран.
+2. Клас Solution має містити три методи.
+3. Клас Solution має містити статичний метод main().
+4. Клас Solution має містити статичний метод метод print() з параметром типу int.
+5. Клас Solution має містити статичний метод метод print() з параметром типу String.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // Напишіть тут ваші методи
+
+    public static void print(int n) {
+
+    }
+
+    public static void print(String str) {
+
+    }
+}
