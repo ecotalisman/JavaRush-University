c175c5ab5b0accfe4e85676971f8ac231b1b7594
ecotalisman <ecotalisman@gmail.com>
2023-12-18 14:33:19 +0200

    Added solution for task 4301 and library Apache Common lang 3.9

---
+++ 3.JavaMultithreading/src/ua/javarush/task/jdk13/task43/task4301/Candidate.java
@@ -0,0 +1,47 @@
+package ua.javarush.task.jdk13.task43.task4301;
+
+/* 
+Порівняй кандидатів
+*/
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.Objects;
+
+public class Candidate {
+
+    private String name;
+    private int age;
+    private final transient String sex;
+    private transient int weight;
+    private transient int height;
+    private int yearsExperience;
+
+    public Candidate(String name, int age, String sex, int weight, int height, int yearsExperience) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.weight = weight;
+        this.height = height;
+        this.yearsExperience = yearsExperience;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        //напишіть тут ваш код
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Candidate candidate = (Candidate) obj;
+
+        return EqualsBuilder.reflectionEquals(this, candidate, true, null, "name",
+                "age", "height", "weight");
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sex, yearsExperience);
+    }
+}
