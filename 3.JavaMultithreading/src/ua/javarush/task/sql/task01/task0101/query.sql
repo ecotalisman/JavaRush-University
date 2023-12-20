aa42b09bedef41a4e6de170dfb885df8ee1ec732
ecotalisman <ecotalisman@gmail.com>
2023-12-20 15:32:05 +0200

    Added solution for task 0101

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task01/task0101/query.sql
@@ -0,0 +1,9 @@
+/*
+Перший запит
+Вибрати вміст колонок id, name, age (у вказаному порядку) з таблиці employee.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT id, name, age FROM  employee;
