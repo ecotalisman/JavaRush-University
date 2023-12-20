c7fe2ac19e4026248287c09da74c80b456e9c578
ecotalisman <ecotalisman@gmail.com>
2023-12-20 22:16:03 +0200

    Added solution for task 0130

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task01/task0130/query.sql
@@ -0,0 +1,10 @@
+/*
+Оператор LIMIT
+Вибрати з таблиці car стовпчики brand, model, quantity (у вказаному порядку). Вибрати потрібно лише
+перших 115 записів. Потрібно використовувати: LIMIT.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT brand, model, quantity FROM car LIMIT 115;
