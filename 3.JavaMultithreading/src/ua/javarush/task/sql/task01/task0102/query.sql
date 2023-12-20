953f7e802b3df5231b34c3f6837ce99b841070fb
ecotalisman <ecotalisman@gmail.com>
2023-12-20 16:38:37 +0200

    Added solution for task 0102

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task01/task0102/query.sql
@@ -0,0 +1,9 @@
+/*
+Вибір даних за колонками
+Вибрати вміст колонок id, name, smth (у вказаному порядку) з таблиці employee.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT id, name, smth FROM employee;
