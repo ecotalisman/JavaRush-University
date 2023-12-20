dbe825ad81a607b2ae4f9a0623c00cc9216a0395
ecotalisman <ecotalisman@gmail.com>
2023-12-20 16:41:28 +0200

    Added solution for task 0103

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task01/task0103/query.sql
@@ -0,0 +1,9 @@
+/*
+Вибір даних за колонками
+Вибрати вміст колонок id, brand_logo, brand_name (у вказаному порядку) з таблиці brand.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT id, brand_logo, brand_name FROM brand;
