e3e9131feb6df3435644cd6d20de8f3285167f23
ecotalisman <ecotalisman@gmail.com>
2023-12-20 22:52:42 +0200

    Added solution for task 0138

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task01/task0138/query.sql
@@ -0,0 +1,10 @@
+/*
+Сортування результату (ORDER BY)
+Вибрати всі стовпчики (використовуй *) з таблиці ip2country. Результат відсортуй за колонкою ip_from.
+Потрібно використати: ORDER BY.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT * FROM ip2country ORDER BY ip_from;
