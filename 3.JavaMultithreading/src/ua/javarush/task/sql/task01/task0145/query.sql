54ff365be16c102397865ccc265a9b6c8dfe1a7a
ecotalisman <ecotalisman@gmail.com>
2023-12-20 23:21:47 +0200

    Added solution for task 0145

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task01/task0145/query.sql
@@ -0,0 +1,9 @@
+/*
+Дії з NULL
+Вибрати всі колонки (використовуй *) з таблиці parts, у яких значення поля description дорівнює NULL.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT * FROM parts WHERE description IS NULL;
