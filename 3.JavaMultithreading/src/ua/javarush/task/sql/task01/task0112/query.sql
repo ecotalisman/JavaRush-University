80547fd81760db5fb8270745cf7bc1e5d7d6c1f9
ecotalisman <ecotalisman@gmail.com>
2023-12-20 17:31:08 +0200

    Added solution for task 0112

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task01/task0112/query.sql
@@ -0,0 +1,10 @@
+/*
+Оператор менше дорівнює
+Вибрати вміст колонок id, salary, department (у вказаному порядку) з таблиці employee, у яких значення поля salary
+менше або дорівнює 1594.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT id, salary, department FROM employee WHERE salary <= 1594;
