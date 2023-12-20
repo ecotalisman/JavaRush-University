0bfde6676cc26096bfd08da26f84ac2c1d64334a
ecotalisman <ecotalisman@gmail.com>
2023-12-20 22:38:05 +0200

    Added solution for task 0136

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task01/task0136/query.sql
@@ -0,0 +1,9 @@
+/*
+Оператор DISTINCT
+Вибрати всі унікальні значення колонки country_code із таблиці ip2country. Потрібно використовувати: DISTINCT.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT DISTINCT country_code FROM ip2country;
