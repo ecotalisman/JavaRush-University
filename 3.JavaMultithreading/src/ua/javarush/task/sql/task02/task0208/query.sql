b55347f13a1d0b44f35fbdebbc92ca754a597893
ecotalisman <ecotalisman@gmail.com>
2023-12-21 14:09:16 +0200

    Added solution for tasks 0204 - 0208

---
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task02/task0208/query.sql
@@ -0,0 +1,9 @@
+/*
+IFNULL з текстом
+Напиши запит, який за допомогою IFNULL перевіряє null на null і повертає 'yes'.
+
+Requirements:
+1. Запит має бути реалізований згідно з умовою.
+*/
+-- Write your code here:
+SELECT IFNULL(NULL, 'yes');
