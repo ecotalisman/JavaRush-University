e3c149e40df3acab9f669e9be00053b136d2f40a
ecotalisman <ecotalisman@gmail.com>
2024-01-19 22:33:45 +0100

    Added solution for task 0345

--- 3.JavaMultithreading/src/ua/javarush/task/sql/task03/task0345/query.sql
+++ 3.JavaMultithreading/src/ua/javarush/task/sql/task03/task0345/query.sql
@@ -12,3 +12,7 @@ Requirements:
 1. Запит має бути реалізований згідно з умовою.
 */
 -- Write your code here:
+SELECT last_name
+FROM film_directors
+         LEFT JOIN (SELECT grossed, director_id FROM films WHERE grossed > 100) AS film
+                   ON film_directors.id = film.director_id WHERE film_directors.country = 'UK';
